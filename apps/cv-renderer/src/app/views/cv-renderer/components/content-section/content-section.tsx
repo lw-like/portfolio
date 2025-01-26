e906cbe0972198812b9bb93d1bed73a750b4df18
Like <wrobelllukasz@gmail.com>
2025-01-26 17:14:49 +0100

    Refactor layout and styles for header and content sections; update viewport meta tag and add print styles

--- apps/cv-renderer/src/app/views/cv-renderer/components/content-section/content-section.tsx
+++ apps/cv-renderer/src/app/views/cv-renderer/components/content-section/content-section.tsx
@@ -1,8 +1,8 @@
 export function ContentSection() {
   return (
-    <div>
+    <section className="p-8 pl-16 pr-16">
       <h1>Welcome to ContentSection!</h1>
-    </div>
+    </section>
   );
 }
 
--- apps/cv-renderer/src/app/views/cv-renderer/components/header-section/header-section.tsx
+++ apps/cv-renderer/src/app/views/cv-renderer/components/header-section/header-section.tsx
@@ -1,8 +1,26 @@
 export function HeaderSection() {
+  const roleStyles = {
+    transform: 'translateY(6rem)',
+  }
+
   return (
-    <div>
-      <h1>Welcome to HeaderSection!</h1>
-    </div>
+    <header className="p-8 pl-16 pr-16 flex w-full bg-stone-950 text-stone-50 shadow-xl">
+      <div>
+        <h1 className="w-1/5 text-6xl/tight whitespace-break-spaces">
+          Test test
+        </h1>
+      </div>
+      <div className="flex flex-1 flex-col justify-items-end text-sm text-right"> 
+        <p><span>tel: </span>+00 000 000 000</p>
+        <p><span>email: </span>aaaaa@aaaaa.aa</p>
+        
+        <span className="flex-1"></span>
+
+        <div className="flex-1 jersey-10-regular text-right" style={roleStyles}>
+          <p><span className="block">FRONTEND</span> <span className="text-stone-950 jersey-10-regular">DEVELOPER</span></p>
+        </div>
+      </div>
+    </header>
   );
 }
 
